2a027e9487e7e9621596fd1e3e386a7b85e1cc82
Kiley Carson <38968995+crawford134@users.noreply.github.com>
2020-12-22 22:44:25 -0700

    Update main.java

--- main.java
+++ main.java
@@ -3,3 +3,21 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader; 
 import javafx.scene.Parent; 
 import javafx.scene.Scene; 
+
+
+void start(Stage stage) throws Exception {
+  
+  root = FXMLLoader.load(getClass().getResource("FXML1.fxml"));
+  
+  Scene scene = new Scene (root); 
+  stage.setScene(scene);
+  stage.show();
+}
+
+
+/**
+*   Launches the program 
+*/
+public static void main(String[] args){
+  launch(args);
+}
