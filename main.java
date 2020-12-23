39d56e000b0fa1a7ccc03ff5f8e45dce299b5b55
Kiley Carson <crawford.carson134@gmail.com>
2020-12-23 12:58:07 -0700

    Updated main.java
    Also changed the name of the associated FXML file

--- main.java
+++ main.java
@@ -1,23 +1,39 @@
-
-import javafx.application.Application; 
-import javafx.fxml.FXMLLoader; 
-import javafx.scene.Parent; 
-import javafx.scene.Scene; 
-
-
-void start(Stage stage) throws Exception {
-  
-  root = FXMLLoader.load(getClass().getResource("FXML1.fxml"));
-  
-  Scene scene = new Scene (root); 
-  stage.setScene(scene);
-  stage.show();
-}
-
-
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+ 
 /**
-*   Launches the program 
-*/
-public static void main(String[] args){
-  launch(args);
+To compile:  javac --module-path $PATH_TO_FX --add-modules javafx.controls main.java
+To run: java --module-path $PATH_TO_FX --add-modules javafx.controls main
+ */
+public class main extends Application {
+    
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+    @Override
+    public void start(Stage primaryStage) {
+        Parent root = FXMLLoader.load(getClass().getResource("mainFXML.fxml"));
+        primaryStage.setTitle("Hello World!");
+        Button btn = new Button();
+        btn.setText("Say 'Hello World'");
+        btn.setOnAction(new EventHandler<ActionEvent>() {
+ 
+            @Override
+            public void handle(ActionEvent event) {
+                System.out.println("Hello World!");
+            }
+        });
+        
+        StackPane root = new StackPane();
+        root.getChildren().add(btn);
+        primaryStage.setScene(new Scene(root, 300, 250));
+        primaryStage.show();
+    }
+    
 }
